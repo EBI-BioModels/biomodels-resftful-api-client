72e0a8477a29df2dd08cb50997b44d5e512727e7
Tung Nguyen <nvntung@gmail.com>
2023-04-13 22:02:26 +0100

    Implement the services of getting model-related content

--- src/biomodels_restful_api_client/services.py
+++ src/biomodels_restful_api_client/services.py
@@ -1,2 +1,48 @@
-def get_model_info(model_id):
-    return model_id
+import os
+import requests
+
+from .constants import API_URL
+
+
+# GET /{model_id}?format=json|xml
+def get_model_info(model_id, out_format="json"):
+    response = requests.get(API_URL + "/" + model_id + "?format=" + out_format)
+    if out_format == "xml":
+        # todo: implement me
+        output = None
+    else:
+        output = response.json()
+    return output
+
+
+# GET /model/files/{model_id}
+def get_model_files_info(model_id, out_format="json"):
+    response = requests.get(API_URL + "/model/files/" + model_id + "?format=" + out_format)
+    if out_format == "xml":
+        # todo: implement me
+        output = None
+    else:
+        output = response.json()
+    return output
+
+
+# GET /model/identifiers
+def get_model_identifiers(out_format="json"):
+    response = requests.get(API_URL + "/model/identifiers?format=" + out_format)
+    return response.json()
+
+
+# GET /model/download/{model_id}
+def download(model_id, filename = None):
+    DOWNLOAD_URL = API_URL + "/model/download/" + model_id
+    local_file = filename
+    if filename is not None:
+        response = requests.get(DOWNLOAD_URL + "?filename=" + filename)
+    else:
+        response = requests.get(DOWNLOAD_URL)
+
+    # Save the file data to the local file
+    with open(local_file, 'wb') as file:
+        file.write(response.content)
+
+    return os.path.abspath(local_file)
--- test/test_services.py
+++ test/test_services.py
@@ -1,8 +1,53 @@
+import os
+
 import utils4test
+
 utils4test.load_path()
-from src.biomodels_restful_api_client import services
+from src.biomodels_restful_api_client import services as bmservices
+import xml.etree.ElementTree as ET
 
 
 def test_get_model_info():
-    model_id = "BIOMD0000000200"
-    assert model_id == services.get_model_info(model_id)
+    model_id = "BIOMD0000000500"
+    out_format = "json"
+    json = bmservices.get_model_info(model_id, out_format)
+    actual_model_name = "Begitt2014 - STAT1 cooperative DNA binding - single GAS polymer model"
+    assert actual_model_name == json["name"]
+    actual_model_format = "SBML"
+    assert actual_model_format == json["format"]["name"]
+
+    # out_format = "xml"
+    # xml = bmservices.get_model_info(model_id, out_format)
+    # actual_model_name = "Begitt2014 - STAT1 cooperative DNA binding - single GAS polymer model"
+    # assert actual_model_name == xml["name"]
+    # actual_model_format = "SBML"
+    # assert actual_model_format == xml["format"]["name"]
+
+
+def test_get_model_files_info():
+    model_id = "BIOMD0000000500"
+    out_format = "json"
+    json = bmservices.get_model_files_info(model_id, out_format)
+    actual_main_file_name = "BIOMD0000000500_url.xml"
+    actual_main_file_size = "83955"
+    assert json["main"][0]["name"] == actual_main_file_name
+    assert json["main"][0]["fileSize"] == actual_main_file_size
+
+
+def test_get_model_identifiers():
+    json = bmservices.get_model_identifiers()
+    model_ids = json["models"]
+    assert "BIOMD0000000500" in model_ids
+
+
+def test_download():
+    model_id = "BIOMD0000000500"
+    main_file_name = "BIOMD0000000500_url.xml"
+
+    download_file_path = bmservices.download(model_id, main_file_name)
+
+    actual_download_file_name = os.path.basename(download_file_path).split('/')[-1]
+
+    assert actual_download_file_name == main_file_name
+
+    os.remove(download_file_path)
